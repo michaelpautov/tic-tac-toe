9fb0f089eb6a8f55a7cb8897f52ed677fdfe7178
michaelpautov <michael.pautov@gmail.com>
2017-11-22 18:12:44 +0600

    Added types and interfaces

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,16 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+
 interface NodeModule {
   id: string;
 }
+
+interface TEMoveInterface {
+  index?: number;
+  score?: number;
+}
+
+
+type TEPlayerSymbolInterface = 'X' | 'O' | 'W';
+type TECellInterface = 'X' | 'O' | 'W' | number;
+
